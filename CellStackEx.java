cb1238fb017df43e6dc5c82e4f9a4d98a1ca26d3
tjdanr747 <tjdanr747@gmail.com>
2026-06-04 13:56:07 +0900

    14시 세이브

---
+++ CellStackEx.java
@@ -0,0 +1,12 @@
+public class CellStackEx {
+    public static void main(String[] args) {
+        firstMethod();
+    }
+    static void firstMethod() {
+        secondMethod();
+    }
+    static void secondMethod() {
+        System.out.println("secondMethod()");
+     }
+    
+}
